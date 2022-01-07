70b01ff37fa9f1b9c4b0127afaa7142d429b92b2
khushxxl <khushaal.choithramani@gmail.com>
2022-01-08 01:20:42 +0530

    website v-12

--- data.js
+++ data.js
@@ -28,6 +28,19 @@ export const projectData = [
   },
 
   {
+    id: 2,
+    name: 'Full Stack Instagram Clone',
+    description:
+      'Full stack instagram clone with features like image uploading and full proof authentication! (Web) ',
+    tech:
+      'Nextjs , Reactjs , Recoiljs , Firebase Auth , Firebase Firestore , Tailwind CSS ',
+    image:
+      'https://user-images.githubusercontent.com/73185436/144228318-61767ba7-2cab-4b96-8f22-69efa5344e6d.png',
+    gitRepoLink: 'https://github.com/khushxxl/instagram-nextjs',
+    deployedLink: 'https://instagram-nextjs-khushxxl.vercel.app/',
+  },
+
+  {
     id: 1,
     name: 'Full Stack Web3 Chat dApp ',
     description:
@@ -48,18 +61,6 @@ export const projectData = [
       'https://user-images.githubusercontent.com/73185436/144180437-3f95d3ab-c9c1-4ee9-a315-7fc2196fd493.png',
     gitRepoLink: 'https://github.com/khushxxl/rn-ecom-app',
     deployedLink: 'https://github.com/khushxxl/rn-ecom-app',
-  },
-  {
-    id: 2,
-    name: 'Full Stack Instagram Clone',
-    description:
-      'Full stack instagram clone with features like image uploading and full proof authentication! (Web) ',
-    tech:
-      'Nextjs , Reactjs , Recoiljs , Firebase Auth , Firebase Firestore , Tailwind CSS ',
-    image:
-      'https://user-images.githubusercontent.com/73185436/144228318-61767ba7-2cab-4b96-8f22-69efa5344e6d.png',
-    gitRepoLink: 'https://github.com/khushxxl/instagram-nextjs',
-    deployedLink: 'https://instagram-nextjs-khushxxl.vercel.app/',
   },
 
   {
