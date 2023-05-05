a71f73a37b3cbc93ad15314e3a760d36594eb5cd
khushxxl <khushaal.choithramani@gmail.com>
2023-05-05 16:08:09 +0100

    links added

--- components/LatestWork.js
+++ components/LatestWork.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 
 const LatestWork = () => {
   return (
-    <div className="bg-[#03001E] flex flex-col items-center justify-center">
+    <div className="bg-[#03001E] flex flex-col items-center justify-center place-content-center">
       <p className="text-white font-style-4 text-center xl:text-center tracking-wider flex flex-col items-center justify-center font-bold text-5xl mt-24">
         Recent Work ✨
       </p>
-      <div className="flex flex-col xl:flex-row w-full justify-center space-x-10 text-left items-center md:items-center align-middle place-items-center">
+      <div className="flex flex-col xl:flex-row w-full justify-center space-x-10 text-left items-center md:items-center align-middle self-center place-items-center">
         <div className="">
           <p className="text-white font-semibold tracking-widest font-style-4 text-center xl:text-left text-4xl">
             Viia
@@ -23,7 +23,7 @@ const LatestWork = () => {
             Available on Playstore & App Store
           </p>
           <div className=" bg-blue-400 mx-10 md:mx-0 align-middle md:w-fit p-2 mt-5 rounded-lg justify-center items-center">
-            <Link href={''}>
+            <Link href={'https://apps.apple.com/gb/app/viia/id6447322346'}>
               <p className="text-black font-semibold cursor-pointer tracking-widest text-center xl:text-left text-2xl ">
                 Install Now
               </p>
@@ -32,12 +32,15 @@ const LatestWork = () => {
         </div>
 
         <div className="flex items-center justify-center">
-          <Image
-            src={require('../images/viia.png')}
-            height={500}
-            width={500}
-            objectFit="cover"
-          />
+          <Link href={'https://apps.apple.com/gb/app/viia/id6447322346'}>
+            <Image
+              src={require('../images/viia.png')}
+              height={500}
+              width={500}
+              objectFit="cover"
+              className=""
+            />
+          </Link>
         </div>
       </div>
     </div>
